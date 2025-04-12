fa960b93ad07769533b59d03420961f8712593c1
nitemoss <uhinemine@gmail.com>
2025-04-12 18:21:08 +0300

    Brief page names in navigation

--- app.py
+++ app.py
@@ -5,36 +5,40 @@ st.set_page_config(layout="wide")
 pages = {
     "Andrii": [
         st.Page("andrii/overview.py",
-                title="Dataset",
+                title="Overview",
                 url_path="andrii-overview",
                 icon="📄"),
         st.Page("andrii/exploration.py",
-                title="Exploration",
+                title="Explore",
                 url_path="andrii-exploration",
                 icon="📊"),
     ],
-    "Artyom": [
+    "Artjoms - Airbnb Flat Prices": [
         st.Page("artyom/overview.py",
-                title="Dataset & Exploration",
+                title="Overview",
                 url_path="artyom-overview",
+                icon="📄"),
+        st.Page("artyom/exploration.py",
+                title="Explore",
+                url_path="artyom-exploration",
                 icon="📊"),
     ],
-    "Border Crossings (Edgars)": [
+    "Edgars - US Border Crossings": [
         st.Page("edgars/overview.py",
-                title="Dataset",
+                title="Overview",
                 url_path="edgars-overview",
                 icon="📄"),
         st.Page("edgars/exploration.py",
-                title="Border Crossings Explorer",
+                title="Explore",
                 icon="📊"),
     ],
-    "German rental offers (Georgy)": [
+    "Georgy - Rental Offers in Germany": [
         st.Page("georgy/overview.py",
-                title="Dataset overview",
+                title="Overview",
                 url_path="georgy-overview",
                 icon="📄"),
         st.Page("georgy/exploration.py",
-                title="Exploration",
+                title="Explore",
                 url_path="georgy-exploration",
                 icon="📊"),
     ]
